09f9417b9df3975832092671ce52473a8dcf7fc8
Terver Tatyough <tcubeterver@gmail.com>
2024-02-23 15:02:59 +0400

    PWA first commit

---
+++ products.js
@@ -0,0 +1,57 @@
+let products = [
+  {
+    id: 1001,
+    title: "Cat Food, 25lb bag",
+    description:
+      "A 25 pound bag of <em>irresistible</em>," +
+      "organic goodness for your cat.",
+    price: 2000,
+    image: "cat.png",
+    availableInventory: 10,
+    rating: 2,
+  },
+  {
+    id: 1002,
+    title: "Dog Food, 25lb bag",
+    description:
+      "A 25 pound bag of <em>irresistible</em>," +
+      "organic goodness for your cat.",
+    price: 2000,
+    image: "cat.png",
+    availableInventory: 10,
+    rating: 5,
+  },
+  {
+    id: 1003,
+    title: "Lion Food, 25lb bag",
+    description:
+      "A 25 pound bag of <em>irresistible</em>," +
+      "organic goodness for your cat.",
+    price: 2000,
+    image: "cat.png",
+    availableInventory: 10,
+    rating: 3,
+  },
+  {
+    id: 1004,
+    title: "Pig Food, 25lb bag",
+    description:
+      "A 25 pound bag of <em>irresistible</em>," +
+      "organic goodness for your cat.",
+    price: 2000,
+    image: "cat.png",
+    availableInventory: 10,
+    rating: 3,
+  },
+  {
+    id: 1005,
+    title: "Snake Food, 25lb bag",
+    description:
+      "A 25 pound bag of <em>irresistible</em>," +
+      "organic goodness for your cat.",
+    price: 200,
+    image: "cat.png",
+    availableInventory: 10,
+    rating: 3,
+  },
+];
