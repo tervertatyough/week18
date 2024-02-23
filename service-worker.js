0e6e827452721638001c41ed28b22130f888ae46
Terver Tatyough <tcubeterver@gmail.com>
2024-02-23 15:56:49 +0400

    next commit

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+var cacheName = "petstore-v1";
+var cacheFiles = [
+  "index.html",
+  "product.js",
+  "petstore.webmanifest",
+  "cat.png",
+  "icon-store-512.png",
+];
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("[Service Worker] caching all files");
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      console.log("[Service Worker] fetching resource: " + e.request.url);
+      return (
+        r ||
+        fetch(e.request).then(function (response) {
+          return caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        })
+      );
+    })
+  );
+});
